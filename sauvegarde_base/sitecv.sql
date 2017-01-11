bf423b671b4a8731e4862c1e8e1e8c5a5a142bba
vienendelsur <isola.patrick@gmail.com>
2017-01-11 23:13:24 +0100

    sauve BDD
    sauvebase

--- sauvegarde_base/sitecv.sql
+++ sauvegarde_base/sitecv.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.4.10
 -- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Lun 09 Janvier 2017 à 17:28
--- Version du serveur :  10.1.8-MariaDB
--- Version de PHP :  5.6.14
+-- Client :  localhost:8889
+-- Généré le :  Mer 11 Janvier 2017 à 23:11
+-- Version du serveur :  5.5.42
+-- Version de PHP :  5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,19 +27,27 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `t_competences`;
-CREATE TABLE `t_competences` (
+CREATE TABLE IF NOT EXISTS `t_competences` (
   `id_competence` int(11) NOT NULL,
   `competence` text,
   `utilisateur_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_competences`
 --
 
 INSERT INTO `t_competences` (`id_competence`, `competence`, `utilisateur_id`) VALUES
+(1, 'HTML 5', 0),
+(2, 'coucou', 0),
+(3, 'hello', 0),
+(4, 'grrr', 0),
+(5, 'grrrr', 0),
+(6, 'qsdfqsf', 0),
+(7, 'qsdfqsdf', 0),
+(8, 'qsdfsdf', 0),
 (9, 'HTLM 5', 1),
-(13, 'css 3', 1);
+(10, 'JavaScript', 2);
 
 -- --------------------------------------------------------
 
@@ -48,22 +56,38 @@ INSERT INTO `t_competences` (`id_competence`, `competence`, `utilisateur_id`) VA
 --
 
 DROP TABLE IF EXISTS `t_experiences`;
-CREATE TABLE `t_experiences` (
+CREATE TABLE IF NOT EXISTS `t_experiences` (
   `id_experience` int(11) NOT NULL,
-  `titre_e` text NOT NULL,
-  `dates` text,
-  `sous_titre_e` text,
-  `description` text NOT NULL,
+  `titre_e` varchar(45) NOT NULL,
+  `sous_titre_e` varchar(45) DEFAULT NULL,
+  `dates_f` varchar(45) DEFAULT NULL,
+  `description_f` text NOT NULL,
   `competence_id` int(11) NOT NULL,
   `utilisateur_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_experiences`
 --
 
-INSERT INTO `t_experiences` (`id_experience`, `titre_e`, `dates`, `sous_titre_e`, `description`, `competence_id`, `utilisateur_id`) VALUES
-(1, 'qsfqsdf', 'qsdfqsf', 'qsdfsdf', 'qsdf', 0, 1);
+INSERT INTO `t_experiences` (`id_experience`, `titre_e`, `sous_titre_e`, `dates_f`, `description_f`, `competence_id`, `utilisateur_id`) VALUES
+(1, 'qsfqsdf', 'qsdfsdf', 'qsdfqsf', 'qsdf', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `t_formations`
+--
+
+DROP TABLE IF EXISTS `t_formations`;
+CREATE TABLE IF NOT EXISTS `t_formations` (
+  `id_formation` int(11) NOT NULL,
+  `titre_f` varchar(45) NOT NULL,
+  `sous_titres_f` varchar(45) NOT NULL,
+  `dates_f` varchar(45) NOT NULL,
+  `description_f` text NOT NULL,
+  `utilisateur_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -72,11 +96,11 @@ INSERT INTO `t_experiences` (`id_experience`, `titre_e`, `dates`, `sous_titre_e`
 --
 
 DROP TABLE IF EXISTS `t_loisirs`;
-CREATE TABLE `t_loisirs` (
+CREATE TABLE IF NOT EXISTS `t_loisirs` (
   `id_loisir` int(11) NOT NULL,
   `loisir` text NOT NULL,
   `utilisateur_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_loisirs`
@@ -93,7 +117,7 @@ INSERT INTO `t_loisirs` (`id_loisir`, `loisir`, `utilisateur_id`) VALUES
 --
 
 DROP TABLE IF EXISTS `t_portfolio`;
-CREATE TABLE `t_portfolio` (
+CREATE TABLE IF NOT EXISTS `t_portfolio` (
   `id_portfolio` int(11) NOT NULL,
   `nom_img` varchar(45) DEFAULT NULL,
   `utilisateur_id` int(11) DEFAULT NULL
@@ -106,8 +130,8 @@ CREATE TABLE `t_portfolio` (
 --
 
 DROP TABLE IF EXISTS `t_titres`;
-CREATE TABLE `t_titres` (
-  `idt_titres` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `t_titres` (
+  `id_titres` int(11) NOT NULL,
   `titre_01` varchar(45) DEFAULT NULL,
   `titre_03` varchar(45) DEFAULT NULL,
   `titre_04` varchar(45) DEFAULT NULL,
@@ -123,21 +147,21 @@ CREATE TABLE `t_titres` (
 --
 
 DROP TABLE IF EXISTS `t_titre_cv`;
-CREATE TABLE `t_titre_cv` (
-  `id_titre` smallint(6) NOT NULL,
+CREATE TABLE IF NOT EXISTS `t_titre_cv` (
+  `id_titre_cv` smallint(6) NOT NULL,
   `titre_cv` text NOT NULL,
   `accroche` text NOT NULL,
   `logo` varchar(50) NOT NULL,
   `utilisateur_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_titre_cv`
 --
 
-INSERT INTO `t_titre_cv` (`id_titre`, `titre_cv`, `accroche`, `logo`, `utilisateur_id`) VALUES
-(1, 'Formateur en infographie', 'Longtemps je me suis couché de bonne heure et à peine ma bougie éteinte ...', 'sacpisola8080.jpg', 1),
-(2, 'Formateur en langages web et infographie', 'coucou aussi', 'fleur.jpg', 2);
+INSERT INTO `t_titre_cv` (`id_titre_cv`, `titre_cv`, `accroche`, `logo`, `utilisateur_id`) VALUES
+(1, 'Formateur en infographie', 'coucou', 'test', 1),
+(2, 'Formateur en langages web et infographie', 'coucou aussi', 'test', 2);
 
 -- --------------------------------------------------------
 
@@ -146,7 +170,7 @@ INSERT INTO `t_titre_cv` (`id_titre`, `titre_cv`, `accroche`, `logo`, `utilisate
 --
 
 DROP TABLE IF EXISTS `t_utilisateur`;
-CREATE TABLE `t_utilisateur` (
+CREATE TABLE IF NOT EXISTS `t_utilisateur` (
   `id_utilisateur` int(11) NOT NULL,
   `prenom` varchar(25) NOT NULL,
   `nom` varchar(25) NOT NULL,
@@ -165,14 +189,14 @@ CREATE TABLE `t_utilisateur` (
   `ville` varchar(20) NOT NULL,
   `pays` varchar(25) NOT NULL,
   `note` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `t_utilisateur`
 --
 
 INSERT INTO `t_utilisateur` (`id_utilisateur`, `prenom`, `nom`, `email`, `telephone`, `mdp`, `pseudo`, `avatar`, `age`, `date_naissance`, `sexe`, `etat_civil`, `statut_marital`, `adresse`, `code_postal`, `ville`, `pays`, `note`) VALUES
-(1, 'Patrick', 'Isola', 'patrick.isola@lepoles.com', '06 63 74 11 35', 'dexvagw52', 'pisola', '', 52, '1964-11-18', 'Homme', 'M.', 'célibataire', '16, avenue de Laumière', '75019', 'Paris', 'France', 'ras'),
+(1, 'Patrick', 'Isola', 'patrick.isola@lepoles.com', '06 63 74 11 35', '1234678', 'pisola', '', 52, '1964-11-18', 'Homme', 'M.', 'célibataire', '16, avenue de Laumière', '75019', 'Paris', 'France', 'ras'),
 (2, 'Philippe', 'Allouche', 'fil5@netcourrier.com', '07 68 55 86 30', 'pass', 'fil', 'test', 56, '1960-02-27', 'Homme', 'M.', 'célibataire', '35, rue Doudeauville', '75018', 'Paris', 'France', 'ras');
 
 --
@@ -193,6 +217,12 @@ ALTER TABLE `t_experiences`
   ADD KEY `id_competence` (`utilisateur_id`);
 
 --
+-- Index pour la table `t_formations`
+--
+ALTER TABLE `t_formations`
+  ADD PRIMARY KEY (`id_formation`);
+
+--
 -- Index pour la table `t_loisirs`
 --
 ALTER TABLE `t_loisirs`
@@ -209,13 +239,13 @@ ALTER TABLE `t_portfolio`
 -- Index pour la table `t_titres`
 --
 ALTER TABLE `t_titres`
-  ADD PRIMARY KEY (`idt_titres`);
+  ADD PRIMARY KEY (`id_titres`);
 
 --
 -- Index pour la table `t_titre_cv`
 --
 ALTER TABLE `t_titre_cv`
-  ADD PRIMARY KEY (`id_titre`);
+  ADD PRIMARY KEY (`id_titre_cv`);
 
 --
 -- Index pour la table `t_utilisateur`
@@ -231,17 +261,22 @@ ALTER TABLE `t_utilisateur`
 -- AUTO_INCREMENT pour la table `t_competences`
 --
 ALTER TABLE `t_competences`
-  MODIFY `id_competence` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_competence` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `t_experiences`
 --
 ALTER TABLE `t_experiences`
-  MODIFY `id_experience` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_experience` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `t_formations`
+--
+ALTER TABLE `t_formations`
+  MODIFY `id_formation` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `t_loisirs`
 --
 ALTER TABLE `t_loisirs`
-  MODIFY `id_loisir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_loisir` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `t_portfolio`
 --
@@ -251,17 +286,17 @@ ALTER TABLE `t_portfolio`
 -- AUTO_INCREMENT pour la table `t_titres`
 --
 ALTER TABLE `t_titres`
-  MODIFY `idt_titres` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_titres` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `t_titre_cv`
 --
 ALTER TABLE `t_titre_cv`
-  MODIFY `id_titre` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_titre_cv` smallint(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `t_utilisateur`
 --
 ALTER TABLE `t_utilisateur`
-  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Contraintes pour les tables exportées
 --
